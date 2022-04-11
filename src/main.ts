bf8b316c1d955796fe9abbb7eaa21a33e46d0d9b
jasoncheng <shenjingwa444@qq.com>
2022-04-11 11:49:57 +0800

    完善错误信息和解决方案

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,24 @@ const https = require('https');
 import {appid, appSecret} from './private';
 
 const errorMap = {
-  52003: '用户认证失败',
-  52004: 'error2',
-  52005: 'error3'
+  52000: '成功',
+  52001: '请求超时,请重试 ',
+  52002:'系统错误,请重试',
+  52003: '未授权用户,请检查appid是否正确或者服务是否开通  ',
+  54000: '必填参数为空,请检查是否少传参数 ',
+  54001: '签名错误,请检查您的签名生成方法 ',
+  54003: '访问频率受限,请降低您的调用频率',
+
+  54004: '账户余额不足,请前往管理控制台为账户充值',
+  54005: '长query请求频繁,请降低长query的发送频率，3s后再试 ',
+  58000: '客户端IP非法, 检查个人资料里填写的IP地址是否正确，可前往开发者信息-基本信息修改',
+
+  58001: '译文语言方向不支持,检查译文语言是否在语言列表里',
+
+  58002: '服务当前已关闭,请前往管理控制台开启服务 ',
+  90107: '认证未通过或未生效,请前往我的认证查看认证进度 ',
 };
+
 
 export const translator = (word) => {
   let salt = Math.random();
@@ -45,7 +59,7 @@ export const translator = (word) => {
       }
       const object: BaiduResult = JSON.parse(string);
       if (object.error_code) {
-        console.error(errorMap[object.error_code] || object.error_msg)
+        console.error(errorMap[object.error_code] || object.error_msg);
         process.exit(2);
       } else {
         console.log(object.trans_result[0].dst);

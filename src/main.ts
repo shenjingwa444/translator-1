1c919a58272c837f31707fbd1eaa017d123217b2
jasoncheng <shenjingwa444@qq.com>
2022-04-11 11:31:43 +0800

    处理错误信息

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import * as md5 from 'js-md5';
-import {appid, appSecret} from './private';
 
 const https = require('https');
+import {appid, appSecret} from './private';
+
+const errorMap = {
+  52003: '用户认证失败',
+  52004: 'error2',
+  52005: 'error3'
+};
 
 export const translator = (word) => {
   let salt = Math.random();
@@ -39,11 +45,11 @@ export const translator = (word) => {
       }
       const object: BaiduResult = JSON.parse(string);
       if (object.error_code) {
-        console.error(object.error_msg);
-        process.exit(2)
+        console.error(errorMap[object.error_code] || object.error_msg)
+        process.exit(2);
       } else {
         console.log(object.trans_result[0].dst);
-        process.exit(0)
+        process.exit(0);
       }
     });
   });
